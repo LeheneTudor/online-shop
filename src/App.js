7829793c8d37f0717f79f3da90f01bc39f2c6015
tlehene <tudor.lehene@amplexor.com>
2022-06-21 11:50:10 +0300

    Basic navigation.component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Directory from "./components/directory/directory.component";
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>Shop page</h1>
+      <hr />
+      <Outlet />
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />}></Route>
+      </Route>
     </Routes>
   );
 };
